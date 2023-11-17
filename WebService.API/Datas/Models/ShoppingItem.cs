28d08a38a1bd9f8f9a44e919ec88eb3e20e29521
fm39hz <hitpoint2k3@gmail.com>
2023-11-17 21:22:33 +0700

    added Condition Checking for each promote

--- WebService.API/Datas/Models/ShoppingItem.cs
+++ WebService.API/Datas/Models/ShoppingItem.cs
@@ -2,16 +2,16 @@ using WebService.API.VirtualBase;
 
 namespace WebService.API.Datas.Models;
 
-public class ShoppingItem
+public record ShoppingItem(IEnumerable<IPromoteStrategy> AppliedPromoteStrategy)
 {
 	public required long Id { get; set; }
 	public required Product Target { get; set; }
 	public required int Quantity { get; set; }
 	public bool IsSelected { get; set; }
-	public List<IPromoteStrategy> AppliedPromoteStrategy { get; } = new();
 
 	public double GetFinalPrice()
 	{
-		return AppliedPromoteStrategy.Aggregate(Target.BasePrice, (current, promote) => promote.DoDiscount(current));
+		return AppliedPromoteStrategy.Aggregate(Target.BasePrice,
+			(current, promote) => promote.CheckCondition(Target)? promote.DoDiscount(current) : Target.BasePrice);
 	}
 }
