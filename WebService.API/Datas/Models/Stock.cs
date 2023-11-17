457683e4d9815a95810e06e691d565e7c638ef5e
fm39hz <hitpoint2k3@gmail.com>
2023-11-17 12:03:28 +0700

    Created Stock Object

---
+++ WebService.API/Datas/Models/Stock.cs
@@ -0,0 +1,28 @@
+namespace WebService.API.Datas.Models;
+
+public class Stock
+{
+	private static readonly Stock Instance = new();
+
+	private Stock()
+	{
+	}
+
+	private List<Admin> Admins { get; } = new();
+	private List<Product> AvailableProducts { get; } = new();
+
+	public static List<Product> GetProducts()
+	{
+		return Instance.AvailableProducts;
+	}
+
+	public static void SortProducst()
+	{
+	}
+
+	private static void AddProducts(Product product, Admin adminCredential)
+	{
+		if (!Instance.Admins.Any(admin => admin == adminCredential && admin.CouldAddItem)) return;
+		Instance.AvailableProducts.Add(product);
+	}
+}
