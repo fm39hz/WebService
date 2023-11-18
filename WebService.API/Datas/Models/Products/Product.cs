d399f4f144a517e4c5145ce51152aaa9292eba11
fm39hz <hitpoint2k3@gmail.com>
2023-11-18 21:02:33 +0700

    fixed products NameSpace

--- WebService.API/Datas/Models/Products/Product.cs
+++ WebService.API/Datas/Models/Products/Product.cs
@@ -8,5 +8,4 @@ public record Product(ISpecifications Specifications)
 	public string? Name { get; set; }
 	public string? Description { get; set; }
 	public double BasePrice { get; set; }
-	public ISpecifications Specifications { get; set; } = Specifications;
 }
