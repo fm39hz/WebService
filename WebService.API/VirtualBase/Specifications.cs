9008b20aa7fd3feb39fda22557ba7f35d6f2b4a9
fm39hz <hitpoint2k3@gmail.com>
2023-11-26 21:42:31 +0700

    added TableName

--- WebService.API/VirtualBase/Specifications.cs
+++ WebService.API/VirtualBase/Specifications.cs
@@ -7,6 +7,7 @@ public abstract record Specification
 	public long Id { get; set; }
 	public long ProductId { get; set; }
 	public required Product ProductTarget { get; set; }
+	public string TableName { get; protected init; } = null!;
 
 	public abstract string GetSpec();
 }
