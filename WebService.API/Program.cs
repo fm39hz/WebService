83e7c7ea98e1022922859d879f12c8b62d5c69e9
fm39hz <hitpoint2k3@gmail.com>
2023-12-02 00:56:32 +0700

    hiding firebase authentication for now

--- WebService.API/Program.cs
+++ WebService.API/Program.cs
@@ -1,5 +1,3 @@
-using FirebaseAdmin;
-using Google.Apis.Auth.OAuth2;
 using Microsoft.EntityFrameworkCore;
 using WebService.API.Datas.Context;
 
@@ -27,15 +25,15 @@ if (_app.Environment.IsDevelopment())
 	_app.UseSwagger();
 	_app.UseSwaggerUI();
 }
-var _firebaseCredentials =
-	GoogleCredential.FromFile(
-		Directory.GetCurrentDirectory() +
-		"/FirebaseConfig/client_secret_62748980710-rgotd8v0p88pv9sq9rb3vhnkp4qneoj4.apps.googleusercontent.com.json");
-var firebaseApp = FirebaseApp.Create(new AppOptions
-{
-	Credential = _firebaseCredentials,
-	ProjectId = "62748980710-rgotd8v0p88pv9sq9rb3vhnkp4qneoj4.apps.googleusercontent.com"
-});
+// var _firebaseCredentials =
+// 	GoogleCredential.FromFile(
+// 		Directory.GetCurrentDirectory() +
+// 		"/FirebaseConfig/client_secret_62748980710-rgotd8v0p88pv9sq9rb3vhnkp4qneoj4.apps.googleusercontent.com.json");
+// var firebaseApp = FirebaseApp.Create(new AppOptions
+// {
+// 	Credential = _firebaseCredentials,
+// 	ProjectId = "62748980710-rgotd8v0p88pv9sq9rb3vhnkp4qneoj4.apps.googleusercontent.com"
+// });
 _app.UseHttpsRedirection();
 
 _app.UseAuthorization();
