a9be502bbae8c4e377650cd8511553fceb02826e
fm39hz <hitpoint2k3@gmail.com>
2023-12-17 21:12:48 +0700

    Created Migration for Products

--- WebService.API/Datas/DataContext.cs
+++ WebService.API/Datas/DataContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebService.API.Datas.Models.Products;
 using WebService.API.Datas.Models.Users;
+using WebService.API.VirtualBase.Abstract;
 
 namespace WebService.API.Datas.Context;
 
@@ -11,12 +12,23 @@ public class DataContext : DbContext
 	}
 
 	public DbSet<UserInstance> Users { get; init; } = null!;
-	public DbSet<ProductSet> Products { get; init; } = null!;
+	public DbSet<Product> Products { get; init; } = null!;
 	public DbSet<Vga> Vgas { get; init; } = null!;
 	public DbSet<Cpu> Cpus { get; init; } = null!;
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		modelBuilder.Entity<UserInstance>().HasKey(u => u.Uid);
+		modelBuilder.Entity<Product>()
+		.HasOne(p => p.Cpu)
+		.WithOne(c => c.Product)
+		.HasForeignKey<Product>(p => p.ConcreateId);
+
+		modelBuilder.Entity<Product>()
+		.HasOne(p => p.Vga)
+		.WithOne(v => v.Product)
+		.HasForeignKey<Product>(p => p.ConcreateId);
+
+		base.OnModelCreating(modelBuilder);
 	}
 }
