01ce511450641a8d11d1f1be4b8a77e65e69864a
fm39hz <hitpoint2k3@gmail.com>
2023-12-20 15:08:03 +0700

    Merged Promote to ProductController

--- WebService.API/Controllers/Products/ProductController.cs
+++ WebService.API/Controllers/Products/ProductController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using WebService.API.Datas.Context;
+using WebService.API.Service.Factory;
 using WebService.API.Service.Utils;
 using WebService.API.Virtual.Abstract;
 
@@ -56,5 +57,16 @@ public class ProductController : ControllerBase
 	private bool ItemExists(int id)
 	{
 		return _context.Products.ItemExists(id);
+	}
+
+	[HttpGet("Promoted/{id:int}")]
+	public async Task<ActionResult<double>> GetPromotePrice(int id)
+	{
+		var _product = await _context.Products.FindAsync(id);
+		if (_product is null)
+		{
+			return NotFound();
+		}
+		return _product.GetPromotedPrice(PromoteFactory.Create(_product.Type!));
 	}
 }
